75a1c8e510d2e91fc9adce9ae11779d741c574a2
an2riuz <an2riuz@gmail.com>
2018-11-21 20:19:31 +0200

    7 Paskaita baigta.

--- 7 paskaita 2018 11 19/7 paskaita 2018 11 19/Program.cs
+++ 7 paskaita 2018 11 19/7 paskaita 2018 11 19/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _7_paskaita_2018_11_19
 {
@@ -17,7 +18,7 @@ namespace _7_paskaita_2018_11_19
                 
             }
             Console.ReadKey();
-            */
+            
             // Sarasai (list)
 
             List<int> skaiciai = new List<int>();
@@ -61,6 +62,76 @@ namespace _7_paskaita_2018_11_19
             }
 
             Console.ReadKey();
+            */
+            // uzduotys
+
+            int[] skaiciuRinkinys = {0,1,2,3,4,5,6 };
+            List<int> skaiciai = new List<int>();
+            skaiciai.Add(7);
+            skaiciai.Add(12);
+            skaiciai.Add(18);
+            skaiciai.AddRange(skaiciuRinkinys);
+            skaiciai.Insert(3, 85);
+
+            skaiciai.Remove(12);
+            skaiciai.RemoveRange(1,3);
+            skaiciai.RemoveAt(5);
+
+            // ideti 100 atsitiktiniu skaiciu
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                int randomas = rnd.Next(1, 150);
+                skaiciai.Add(randomas);
+            }
+            // 4)Išveskite visą informaciją kurią tik sugalvojate apie šį skaičių sąrašą (suma, vidurkis, kiekis...).
+
+            skaiciai.ForEach(Console.WriteLine);
+            Console.WriteLine("skaiciu suma lygi: " + skaiciai.Sum());
+            Console.WriteLine("skaiciu vidurkis lygus: " + skaiciai.Average());
+            Console.WriteLine("skaiciu kiekis lygus: " + skaiciai.Count);
+            Console.WriteLine("hash kodas: " + skaiciai.GetHashCode());
+            Console.WriteLine("saraso tipas: " + skaiciai.GetType());
+
+            // 5)Suraskite kiekį skaičių, kurie < 20.
+
+            int elementuSkaicius = 0;
+            foreach (var elementas in skaiciai)
+            {
+                if (elementas < 20 )
+                {
+                    elementuSkaicius++;
+                }
+            }
+
+            Console.WriteLine("skaiciu mazesniu uz 20 kiekis lygus: " + elementuSkaicius);
+
+            // 6)Pašalinkite skaičius, kurie > 80;
+
+            for (int i = 0; i < skaiciai.Count; i++)
+            {
+                int elementas = skaiciai[i];
+                if (elementas > 80)
+                {
+                    skaiciai.Remove(elementas);
+                }
+            }
+            Console.WriteLine(skaiciai[25]);
+
+            // 7)Atspausdinkite visus lyginius skaičius į vieną eilutę. (naudojant ForEach() funkciją).
+
+            foreach (var elementas in skaiciai)
+            {
+                if (elementas % 2 == 0)
+                {
+                    Console.Write(elementas + " ");
+                }
+            }
+
+            Console.ReadKey();
+            
+            
+
         }
     }
 }
